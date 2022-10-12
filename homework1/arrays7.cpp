a438fb329b8603f890e4c08baa7ba3b2315bb451
Gokrusk <nigelljama@gmail.com>
2022-10-11 22:32:57 -0500

    arrays

--- homework1/arrays7.cpp
+++ homework1/arrays7.cpp
@@ -7,8 +7,8 @@ using namespace std;
 // Prototipos
 void leerV(int a[], int n);
 void imprimirV(int a[], int n);
-void eleminarRepetido(int a[], int n, int x, int cant);
-void elementosRepetidos(int a[], int n);
+void eleminarRepetido(int a[], int *n, int x, int cant);
+void elementosRepetidos(int a[], int *n);
 void solucion();
 
 int main()
@@ -37,27 +37,27 @@ void imprimirV(int a[], int n) // imprimir vector(array, nelementos)
     }
     cout << "]" << endl;
 }
-void eleminarRepetido(int a[], int n, int x, int cant)
+void eleminarRepetido(int a[], int *n, int x, int cant)
 {
     int j = 0;
     while (j < cant - 1) // repite nveces se encuentre repetido un elemento
     {
-        for (int i = x; i < n; i++) // inicio: elemento repetido
+        for (int i = x; i < *n; i++) // inicio: elemento repetido
         {
             a[i] = a[i + 1]; // asigna la siguiente posicion a la posicion del elemento repetido
         }
-        a[n - 1] = {};
+        *n -= 1;
         j++;
     }
 }
-void elementosRepetidos(int a[], int n) // buscar elementos repetidos
+void elementosRepetidos(int a[], int *n) // buscar elementos repetidos
 {
     int aux, cont = 0;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < *n; i++)
     {
         aux = a[i]; // elemento anclado
         cont = 0;
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < *n; j++)
         {
             if (aux == a[j]) // comproband elemento anclado con todos los elementos
             {
@@ -77,8 +77,8 @@ void solucion()
     cout << "Ingrese la cantidad de elementos a ingresar en vector: ";
     cin >> l;
     int a[l];
-    leerV(a, l);              // Lectura elementos de vector
-    imprimirV(a, l);          // Imprimir vector
-    elementosRepetidos(a, l); // Elemento repetido
-    imprimirV(a, l);          // Imprimir vector final
+    leerV(a, l);               // Lectura elementos de vector
+    imprimirV(a, l);           // Imprimir vector
+    elementosRepetidos(a, &l); // Elemento repetido
+    imprimirV(a, l);
 }
