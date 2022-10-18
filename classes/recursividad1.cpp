f4db8b13e4adfa9fa160731b598eb351b35ad3f1
gokrusk <nigelljama@gmail.com>
2022-10-17 20:51:23 -0500

    recursividad

--- classes/recursividad1.cpp
+++ classes/recursividad1.cpp
@@ -1,25 +1,26 @@
-//4. Programa que imprime un mensaje repetidamente
-#include<iostream>
+// 4. Programa que imprime un mensaje repetidamente
+#include <iostream>
 using namespace std;
 #include "utilidades.h"
 
-//PROTOTIPOS
+// PROTOTIPOS
 void imprimirMensaje(int n);
 int main()
 {
 	int n;
-	cout<<"Ingrese la cantidad de mensajes a imprimir"<<endl;
-	n = leerN(-32768,32768);	//lectura cantidad de repeticiones
-	imprimirMensaje(n);	//impresion
-	cout<<endl;
+	cout << "Ingrese la cantidad de mensajes a imprimir" << endl;
+	n = leerN(-32768, 32768); // lectura cantidad de repeticiones
+	imprimirMensaje(n);		  // impresion
+	cout << endl;
 	system("pause");
 }
-//DEFINICION DE FUNCIONES
+// DEFINICION DE FUNCIONES
 void imprimirMensaje(int x)
 {
-	if(x>1)
+	if (x > 1)
 	{
-		imprimirMensaje(x-1);	
-	}	
-	cout<<endl<<x<<". Hola mundo...\t";
+		imprimirMensaje(x - 1);
+	}
+	cout << endl
+		 << x << ". Hola mundo...\t";
 }
--- classes/recursividad2.cpp
+++ classes/recursividad2.cpp
@@ -1,20 +1,21 @@
-//5. Procedimiento recursivo para individualizar digitos
-#include<iostream>
+// 5. Procedimiento recursivo para individualizar digitos
+#include <iostream>
 using namespace std;
 #include "utilidades.h"
-//PROTOTIPOS
+// PROTOTIPOS
 void individualizarDigito(int x);
 int main()
 {
 	int n;
-	n = leerN(-32768,32768);
+	n = leerN(-32768, 32768);
 	individualizarDigito(n);
 }
 void individualizarDigito(int x)
 {
-	if(x>10 || x<-10)
+	if (x > 10 || x < -10)
 	{
-		individualizarDigito(x/10);
+		individualizarDigito(x / 10);
 	}
-	cout<<endl<<"> "<<x%10<<endl;
+	cout << endl
+		 << "> " << x % 10 << endl;
 }
