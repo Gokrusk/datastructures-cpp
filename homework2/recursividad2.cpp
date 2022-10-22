7599c2dea3d0f7a2bf0fe88e42516b1a9fe115c6
Gokrusk <nigelljama@gmail.com>
2022-10-22 10:02:25 -0500

    recusividad

--- homework2/recursividad2.cpp
+++ homework2/recursividad2.cpp
@@ -1,1 +1,46 @@
-//2. Generar las N primeras líneas del triángulo de Pascal
+// 2. Generar las N primeras líneas del triángulo de Pascal
+
+#include <iostream>
+using namespace std;
+#include "utilidades.h"
+// PROTOTIPOS
+void trianguloPascal(int n, int *i);
+void espacios(int r, int n);
+void calculoPascal(int r, int n, int *i);
+
+int main()
+{
+    int n, i = 1;
+    cout << "Cuantas lineas desea ver del triangulo de pascal: ";
+    cin >> n;
+    trianguloPascal(n, &i);
+    return 0;
+}
+
+void trianguloPascal(int n, int *i)
+{
+    if (n > 0)
+    {
+        trianguloPascal(n - 1, i);
+        espacios(n,n);
+        calculoPascal(n, n, i);
+    }
+        cout<<endl;
+}
+void espacios(int r, int n)
+{
+    if (r > 1)
+    {
+        espacios(r-n+1, n);
+        cout << " ";
+    }
+}
+void calculoPascal(int r, int n, int *i)
+{
+    if (r <= n)
+    {
+        calculoPascal(r + 1, n, i);
+        cout << *i << " ";
+        *i *= (n - r) / r;
+    }
+}
--- homework2/recursividad3.cpp
+++ homework2/recursividad3.cpp
@@ -2,3 +2,54 @@
 //valor factorial (producto de los valores menores o iguales al número). 
 //Implemente todos los ajustes necesarios para permitir el cálculo 
 //del factorial también para los valores negativos.
+
+#include<iostream>
+using namespace std;
+#include "utilidades.h"
+
+//PROTOTIPOS
+void leer(int n, int x);
+int factorial(int n);
+int factorial2(int n);
+
+int main()
+{
+    int n,x;
+    cout<<"Ingrese cuantos numeros se calculara el factorial: ";
+    cin>>n;
+    leer(n,x);
+    return 0;
+}
+
+void leer(int n,int x)
+{
+    if(n>0)
+    {
+        leer(n-1,x);
+        x = leerN(-32768, 32768);
+        if(x>0)
+        {
+            cout<<"El factorial de "<<x<<" es: "<<factorial(x)<<endl;
+        }else{
+            cout<<"El factorial de "<<x<<" es: "<<factorial2(x)<<endl;
+        }
+    }
+}
+int factorial(int n)
+{
+    if(n>0)
+    {
+        return n * factorial(n-1);
+    }else{
+        return 1;
+    }
+}
+int factorial2(int n)
+{
+    if(n<0)
+    {
+        return n * factorial2(n+1);
+    }else{
+        return 1;
+    }
+}
