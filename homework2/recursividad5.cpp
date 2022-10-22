8964d07ec435e7ab3a3b734ce9c2ae789f19c865
Gokrusk <nigelljama@gmail.com>
2022-10-22 10:57:15 -0500

    recursividad

--- homework2/recursividad5.cpp
+++ homework2/recursividad5.cpp
@@ -1,36 +1,55 @@
-//5. Determinar si cada número ingresado es un valor primo o compuesto.
+// 5. Determinar si cada número ingresado es un valor primo o compuesto.
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 #include "utilidades.h"
 
-//PROTOTIPOS
+// PROTOTIPOS
 void comprobarPriComp(int n);
-void leer(int n, int x);
-void primComp(int n);
+void leer(int n, int x, int *c, int d);
+void contar(int n, int x, int d, int *c);
+void primComp(int n, int *c);
 
 int main()
 {
-    int n, x;
-    cout<<"Ingrese cuantos numeros a comprobar: ";
-    cin>>n;
-    leer(n,x);
+    int n, x, c=0, d=0;
+    cout << "Ingrese cuantos numeros a comprobar: ";
+    cin >> n;
+    leer(n, x, &c, d);
     return 0;
 }
 
-void leer(int n, int x)
+void leer(int n, int x, int *c, int d)
 {
-    if(n>0)
+    if (n > 0)
     {
-        leer(n-1,x);
+        leer(n - 1, x, c, d);
         x = leerN(-32768, 32768);
+        contar(x, x, d, c);
+        primComp(x, c);
     }
 }
-void primComp(int n)
+void contar(int n, int x, int d, int *c)
 {
-    if(n>0)
+    *c = 0;
+    if (n > 1)
     {
-        primComp(n - 1);
+        contar(n - 1, x, d, c);
     }
-    
+    d = x % n;
+    if(d==0)
+    {
+        *c = *c +1;
+    }
+}
+void primComp(int n, int *c)
+{
+    if (*c == 2)
+    {
+        cout << "El numero "<< n << " es primo"<<endl;
+    }
+    else
+    {
+        cout << "El numero "<< n << " es compuesto"<<endl;
+    }
 }
