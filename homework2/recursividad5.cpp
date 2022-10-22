eb3d4ce6b7be22f3a5199c46e17804fa40ea7dcd
Gokrusk <nigelljama@gmail.com>
2022-10-22 10:01:46 -0500

    recursividad

--- homework2/recursividad5.cpp
+++ homework2/recursividad5.cpp
@@ -1,1 +1,36 @@
 //5. Determinar si cada número ingresado es un valor primo o compuesto.
+
+#include<iostream>
+using namespace std;
+#include "utilidades.h"
+
+//PROTOTIPOS
+void comprobarPriComp(int n);
+void leer(int n, int x);
+void primComp(int n);
+
+int main()
+{
+    int n, x;
+    cout<<"Ingrese cuantos numeros a comprobar: ";
+    cin>>n;
+    leer(n,x);
+    return 0;
+}
+
+void leer(int n, int x)
+{
+    if(n>0)
+    {
+        leer(n-1,x);
+        x = leerN(-32768, 32768);
+    }
+}
+void primComp(int n)
+{
+    if(n>0)
+    {
+        primComp(n - 1);
+    }
+    
+}
