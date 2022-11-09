b776e2fa901482634239ff85cc1db7925a3b1eed
Gokrusk <nigelljama@gmail.com>
2022-11-09 07:06:51 -0500

    cola

--- classes/TDA/cola.h
+++ classes/TDA/cola.h
@@ -1,41 +1,42 @@
-//TDA COLA
+// TDA COLA
 typedef int Tipo;
 #define EXT 20
-class Cola{											//Definicion de clase
-	private:										//Atributos
-			int frente;
-			int final;
-			Tipo elem[EXT];							//Arreglos de valores de la cola
-	public:											//Metodos
-			Cola();
-			void setFrente(int x);					//Actualiza atributo frente
-			void setFinal(int x);					//Actualiza atributo final
-			void setElem(int v, int p);				//Actualiza atributo elem[x,p]
-			int getFrente();						//Retorna atributo frente
-			int getFinal();							//Retorna atributo final
-			Tipo getElem(int p);					//Retorna elemento atributo elem
-			void borrarCola();						//Vuelve a inicializar el TDA
-			bool colaVacia();						//Retorna si la cola esta vacia
-			bool colaLlena();						//Retorna si la cola esta lleno
-			int tamanioCola();						//Retorna tamanio cola
-			Tipo extraerVal();						//Extrae y elimina un elemento del TDA
-			void insertarVal(Tipo x);				//Inserta un elemento en el TDA
-			Tipo frenteCola();						//Devuelve elemento del frente de la cola
+class Cola
+{		 // Definicion de clase
+private: // Atributos
+	int frente;
+	int final;
+	Tipo elem[EXT]; // Arreglos de valores de la cola
+public:				// Metodos
+	Cola();
+	void setFrente(int x);		// Actualiza atributo frente
+	void setFinal(int x);		// Actualiza atributo final
+	void setElem(int v, int p); // Actualiza atributo elem[x,p]
+	int getFrente();			// Retorna atributo frente
+	int getFinal();				// Retorna atributo final
+	Tipo getElem(int p);		// Retorna elemento atributo elem
+	void borrarCola();			// Vuelve a inicializar el TDA
+	bool colaVacia();			// Retorna si la cola esta vacia
+	bool colaLlena();			// Retorna si la cola esta lleno
+	int tamanioCola();			// Retorna tamanio cola
+	Tipo extraerVal();			// Extrae y elimina un elemento del TDA
+	void insertarVal(Tipo x);	// Inserta un elemento en el TDA
+	Tipo frenteCola();			// Devuelve elemento del frente de la cola
 };
 
 Cola::Cola()
 {
-	setFrente(0);									//Inicializr la pos apuntada por frente
-	setFinal(-1);									//Inicializr la pos apuntada por final
+	setFrente(0); // Inicializr la pos apuntada por frente
+	setFinal(-1); // Inicializr la pos apuntada por final
 }
 void Cola::setFrente(int x)
 {
 	frente = x;
-}				
+}
 void Cola::setFinal(int x)
 {
 	final = x;
-}					
+}
 void Cola::setElem(int v, int p)
 {
 	elem[p] = v;
@@ -65,9 +66,8 @@ bool Cola::colaVacia()
 
 bool Cola::colaLlena()
 {
-	return (getFinal() == EXT-1);
+	return (getFinal() == EXT - 1);
 }
-
 
 int Cola::tamanioCola()
 {
@@ -76,11 +76,11 @@ int Cola::tamanioCola()
 
 Tipo Cola::extraerVal()
 {
-	Tipo x;											//var para contener el valor a devolver
-	if(!colaVacia())
+	Tipo x; // var para contener el valor a devolver
+	if (!colaVacia())
 	{
 		x = getElem(getFrente());
-		setFrente(getFrente()+1);
+		setFrente(getFrente() + 1);
 		return x;
 	}
 	return x;
@@ -88,27 +88,20 @@ Tipo Cola::extraerVal()
 
 void Cola::insertarVal(Tipo x)
 {
-	if(!colaLlena())
+	if (!colaLlena())
 	{
-		setFinal(getFinal()+1);
-		setElem(x,getFinal());
+		setFinal(getFinal() + 1);
+		setElem(x, getFinal());
 	}
 }
 
 Tipo Cola::frenteCola()
 {
-	Tipo x;											//var para contener el valor a devolver
-	if(!colaVacia())
+	Tipo x; // var para contener el valor a devolver
+	if (!colaVacia())
 	{
 		x = getElem(getFrente());
-		//return x;
+		// return x;
 	}
 	return x;
 }
-
-
-
-
-
-
-
--- classes/TDA/tda8.cpp
+++ classes/TDA/tda8.cpp
@@ -1,36 +1,35 @@
-//Implementar el uso del TDA Cola
-#include<iostream>
+// Implementar el uso del TDA Cola
+#include <iostream>
 using namespace std;
 #include "utilidades.h"
 #include "cola.h"
 
 int main()
 {
-	Cola c;											//Instancia del TDA Cola
+	Cola c; // Instancia del TDA Cola
 	Tipo val;
 	int n;
-	cout<<"Ingrese la cantidad de elementos a insertar: ";
-	cin>>n;
-	cout<<"1. TAMANIO QUEUE: "<<c.tamanioCola()<<endl;
-	for(int i=1;i<=n;i++)
+	cout << "Ingrese la cantidad de elementos a insertar: ";
+	cin >> n;
+	cout << "1. TAMANIO QUEUE: " << c.tamanioCola() << endl;
+	for (int i = 1; i <= n; i++)
 	{
-		cout<<"Elemento "<<i<<": ";
-		cin>>val;
+		cout << "Elemento " << i << ": ";
+		cin >> val;
 		c.insertarVal(val);
 	}
-	cout<<endl;
-	cout<<"2. TAMANIO QUEUE: "<<c.tamanioCola()<<endl;
-	cout<<" ELEMENTOS IN QUEUE "<<endl;
-	cout<<"[ ";
-	for(int i=0;i<n;i++)
+	cout << endl;
+	cout << "2. TAMANIO QUEUE: " << c.tamanioCola() << endl;
+	cout << " ELEMENTOS IN QUEUE " << endl;
+	cout << "[ ";
+	for (int i = 0; i < n; i++)
 	{
-		cout<<c.extraerVal()<<" ";
+		cout << c.extraerVal() << " ";
 	}
-	cout<<"]"<<endl;
-	
-	cout<<"3. TAMANIO QUEUE: "<<c.tamanioCola()<<endl;
-	
-	cout<<endl;
+	cout << "]" << endl;
+
+	cout << "3. TAMANIO QUEUE: " << c.tamanioCola() << endl;
+
+	cout << endl;
 	system("pause");
 }
-
