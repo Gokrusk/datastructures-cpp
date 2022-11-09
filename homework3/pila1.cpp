a8676470a195610c81d4c54dc959e1b96ef3f301
Gokrusk <nigelljama@gmail.com>
2022-11-09 07:06:36 -0500

    pila

---
+++ homework3/pila1.cpp
@@ -0,0 +1,17 @@
+// Programa que utilice una pila para determinar si cada una de las frases ingresadas por el usuario
+// es un palíndromo (frase cuya lectura directa e indirecta es igual: ana, alila, etc).
+#include <iostream>
+#include <curses.h>
+using namespace std;
+#include "pila.h"
+
+int main()
+{
+    char f;
+    Pila frase;
+    cout << "Ingrese la frase: ";
+
+    //Insertar letras mientras escribes, hasta que presione enter.
+    
+    
+}
