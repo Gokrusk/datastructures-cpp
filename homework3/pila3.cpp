b7d68903985f33dcd7927ce40d2c1b93482d699a
Gokrusk <nigelljama@gmail.com>
2022-11-13 18:08:40 -0500

    pila

--- homework3/pila3.cpp
+++ homework3/pila3.cpp
@@ -1,27 +1,50 @@
-//Escribir un programa para determinar si una secuencia de caracteres ingresada por el usuario 
-//es de la forma: X & Y donde X una cadena de caracteres e Y la cadena inversa. 
-//El carácter & es el separador.
+// Escribir un programa para determinar si una secuencia de caracteres ingresada por el usuario
+// es de la forma: X & Y donde X una cadena de caracteres e Y la cadena inversa.
+// El carácter & es el separador.
 
 #include <iostream>
 #include <curses.h>
 #include <cstring>
+#include "stdio.h"
 using namespace std;
-#include "pila.h"
+#include "pila3.h"
 
 int main()
 {
     // PROTOTIPOS
-    void lecturaNums(Pila * p);
-    void imprimirPila(Pila p);
-    int convertEntero(double a);
-    int contarDigitos(int x);
-    double eliminarElementosDigitosPares(Pila * p);
-    Pila a;
-    lecturaNums(&a);
-    cout << " ELEMENTOS EN PILA " << endl;
-    imprimirPila(a);
-    eliminarElementosDigitosPares(&a);
-    cout << " ELEMENTOS CON CANTIDAD PAR DE DIGITOS ELIMINADOS " << endl;
-    imprimirPila(a);
+    void leerFrase(char a[EXT]);                          // Lectura de frase
+    void apilarFrase(Pila * b, char a[EXT], int tamanio); // Apilar cada caracter en una pila
+    void imprimirPila(Pila c);                            // Imrpimir la pila
+    Pila a,b;
+    int t = 0;
+    char frase[EXT];
+    leerFrase(frase);
+    t = strlen(frase);                                     //Longitud de cadena
+    //cout<<t<<endl;
+    cout<<frase[7]<<endl;
+    //apilarFrase(&a, frase, t);
+    //imprimirPila(a);
     return 0;
+}
+
+void leerFrase(char a[EXT])
+{
+    cout << "Ingrese la frase: ";
+    gets(a);
+}
+void apilarFrase(Pila *b, char a[EXT], int tamanio)
+{
+    for (int i = 0; i < tamanio; i++)
+    {
+        b->push(a[i]);
+    }
+}
+void imprimirPila(Pila c)
+{
+    cout << "[ ";
+    while (!c.pilaVacia())
+    {
+        cout << c.pop() << " ";
+    }
+    cout << "]" << endl;
 }
