30d69554d027f8fbb9e01955929337ea5bd92bcd
gokrusk <nigelljama@gmail.com>
2022-11-21 20:41:19 -0500

    cola

---
+++ homework4/cola1.cpp
@@ -0,0 +1,83 @@
+// Invertir el orden de los números pares contenidos.
+// Tener en cuenta que los demás elementos deben quedar en el mismo orden.
+
+#include <iostream>
+using namespace std;
+#include "cola.h"
+#include "pila.h"
+
+int main()
+{
+    // PROTOTIPOS
+    void ingresarCola(Cola * a);                      // metodo para insertar valores a cola
+    void imprimirCola(Cola a);                        // metodo para imprimir cola
+    Tipo buscarPares(Cola a, Cola * b, Pila p);       // metodo para buscar los pares e invertir
+    Tipo insertarParesInvertidos(Cola * a, Cola * b); // metodo para insertar pares invertidos
+    Cola b, c;
+    Pila p;
+    Tipo val;
+    ingresarCola(&c);
+    imprimirCola(c);
+    cout << endl;
+    buscarPares(c, &b, p);
+    insertarParesInvertidos(&c, &b);
+    imprimirCola(c);
+}
+
+void ingresarCola(Cola *a)
+{
+    Tipo val = 0, fin = 999;
+    do
+    {
+        cout << "Ingrese elemento, (ingrese " << fin << " para finalizar): ";
+        cin >> val;
+        if (val != fin)
+        {
+            a->insertarVal(val);
+        }
+    } while (val != fin);
+}
+
+void imprimirCola(Cola a)
+{
+    cout << "[ ";
+    while (!a.colaVacia())
+    {
+        cout << a.extraerVal() << " ";
+    }
+    cout << "]";
+}
+
+Tipo buscarPares(Cola a, Cola *b, Pila p)
+{
+    Tipo aux;
+    while (!a.colaVacia()) // mientras no este vacia
+    {
+        aux = a.extraerVal(); // extrae un valor, si es par lo inserta en una pila
+        if (aux % 2 == 0)
+        {
+            p.push(aux);
+        }
+    }
+    while (!p.pilaVacia()) // inserta los valores de la pila en una cola
+    {
+        b->insertarVal(p.pop());
+    }
+}
+
+Tipo insertarParesInvertidos(Cola *a, Cola *b)
+{
+    Cola c;
+    Tipo aux1, aux2;
+    while (!a->colaVacia()) // mientras cola no este vacia
+    {
+        aux1 = a->extraerVal();
+        if (aux1 % 2 == 0) // si el valor es par inserta en una cola los valores pares invertidos
+        {
+            c.insertarVal(b->extraerVal());
+        }
+        else
+            c.insertarVal(aux1); // si no inserta los valores que no son pares sin afectar su orden
+    }
+    *a = c;
+}
