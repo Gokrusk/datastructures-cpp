85ed9e7fd8ea216882ef4919e0dfdf5e5265be4a
gokrusk <nigelljama@gmail.com>
2022-11-22 12:50:40 -0500

    lista

--- classes/TDA/Listas/lista.h
+++ classes/TDA/Listas/lista.h
@@ -1,18 +1,22 @@
 // ARCHIVO DE CABECERA QUE CONTIENE LA IMPLEMENTACION DEL TDA LISTA
 #include "nodo.h" //inclusion de la definicion de nodos
-#include<iostream>
+#include <iostream>
 
 class Lista // definicion del TDA Lista
 {
-private:                      // atributos
-    Nodo *primero;            // apunta al primer nodo
-public:                       // metodos
-    Lista();                  // constructor
-    void setPrimero(Nodo *x); // actualizar el estado del atributo primero
+private:                             // atributos
+    Nodo *primero;                   // apunta al primer nodo
+public:                              // metodos
+    Lista();                         // constructor
+    void setPrimero(Nodo *x);        // actualizar el estado del atributo primero
+    void insertarNodoInicio(Tipo x); // inserta un nuevo valor como primer nodo de la lista
+    Nodo *ultimoNodo(); //devuelve la direccion del ultimo nodo
+    void anular();  // metodo que vuelve a inicializar una lista
+    bool esVacia(); // devuelve la lista esta vacia
     Nodo *getPrimero();
 };
 
-Lista::Lista()                  // la lista empieza sin nodos
+Lista::Lista() // la lista empieza sin nodos
 {
     setPrimero(NULL);
 }
@@ -25,4 +29,33 @@ void Lista::setPrimero(Nodo *x)
 Nodo *Lista::getPrimero()
 {
     return primero;
+}
+
+void Lista::insertarNodoInicio(Tipo x)
+{
+    Nodo *nuevo; //  var para almacenar direccion de memoria de nuevos nodos a crear
+    nuevo = new Nodo(x, getPrimero());
+    setPrimero(nuevo);
+}
+
+bool Lista::esVacia() // devuelve la lista esta vacia
+{
+    return (getPrimero() == NULL);
+}
+
+void Lista::anular()
+{
+    setPrimero(NULL);
+}
+
+Nodo *Lista::ultimoNodo()
+{
+    // recorrido de la lista
+    Nodo *actual;                     // referencia a nodo actual de la lista
+    actual = getPrimero();            // almacena temporalmente la posicion del primer nodo
+    while (actual->getPunt() != NULL) // repetir mientras no se alcance el final de la lista
+    {
+        actual = actual->getPunt(); // desplaza el puntero actual al siguiente nodo de la lista
+    }
+    return actual;
 }
--- classes/TDA/Listas/lista1.cpp
+++ classes/TDA/Listas/lista1.cpp
@@ -3,32 +3,48 @@ using namespace std;
 
 int main()
 {
-    Lista l;             // creacion del objeto lista
-    Tipo x;
-    // l.setPrimero(&n1); // incluir nodo a lista
-    // Nodo n2(10,&n1);
-    // l.setPrimero(&n2);
-    // Nodo n3(20,&n2);
-    // l.setPrimero(&n3);
-    
-    for (int i = 0; i < 5; i++)
+    void imprimirLista(Lista a); // presenta los valores contenidos en los nodos de una lista
+    void leerLista(Lista * a);   // insertar datos en una lista
+    Lista l;                     // creacion del objeto lista
+    cout << endl
+         << "La lista " << (l.esVacia() ? "si " : "no ") << " esta vacia" << endl;
+    leerLista(&l);
+    cout << endl
+         << "La lista " << (l.esVacia() ? "si " : "no ") << " esta vacia" << endl;
+    imprimirLista(l);
+    cout << "Primer valor contenido en la lista es: " << l.getPrimero()->getDato() << endl;
+    cout << "Ultimo valor contenido en la lista es: " << l.ultimoNodo()->getDato() << endl;
+    l.anular();
+    cout << endl
+         << "La lista " << (l.esVacia() ? "si " : "no ") << " esta vacia" << endl;
+
+    return 0;
+}
+
+void leerLista(Lista *a) // insertar datos en una lista
+{
+    Tipo x, fin = 999;
+    do
     {
-        cout<<"Ingrese valor para la lista: ";
-        cin>>x;
-        Nodo nuevo(x,l.getPrimero());
-        l.setPrimero(&nuevo);
-    }
-    
+        cout << "Ingrese valor para la lista (fin 999): ";
+        cin >> x;
+        if (x != fin)
+        {
+            a->insertarNodoInicio(x);
+        }
+    } while (x != fin);
+}
+void imprimirLista(Lista a)
+{
     // recorrido de la lista
     Nodo *actual; // referencia a nodo actual de la lista
     cout << endl
          << " VALORES ALMACENADOS EN LA LISTA " << endl;
-    actual = l.getPrimero(); // almacena temporalmente la posicion del primer nodo
+    actual = a.getPrimero(); // almacena temporalmente la posicion del primer nodo
     while (actual != NULL)   // repetir mientras no se alcance el final de la lista
     {
         cout << "> " << actual->getDato() << endl;
         actual = actual->getPunt(); // desplaza el puntero actual al siguiente nodo de la lista
     }
     cout << endl;
-    return 0;
 }
