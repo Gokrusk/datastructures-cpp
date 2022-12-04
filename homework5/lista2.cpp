6818d00294d461c5438ae0cfd0b072316590d527
gokrusk <nigelljama@gmail.com>
2022-12-04 14:37:47 -0500

    l2

--- homework5/lista2.cpp
+++ homework5/lista2.cpp
@@ -1,1 +1,44 @@
 // Imprimir los elementos de una lista, en el sentido inverso al que fueron ingresados
+#include "lista.h"
+using namespace std;
+
+int main()
+{
+    // PROTOTIPOS
+    void leerLista(Lista * a);                        // insertar datos en una lista
+    void imprimirLista(Lista a);                      // presenta los valores contenidos en los nodos de una lista
+    Lista l;
+    leerLista(&l);
+    imprimirLista(l);
+    //insertarElementos(l);
+    cout << " LISTA " << endl;
+    imprimirLista(l);
+    return 0;
+}
+void leerLista(Lista *a) // insertar datos en una lista
+{
+    Tipo x, fin = 999;
+    do
+    {
+        cout << "Ingrese valor para la lista (fin 999): ";
+        cin >> x;
+        if (x != fin)
+        {
+           a->insertarNodoInicio(x);
+        }
+    } while (x != fin);
+}
+void imprimirLista(Lista a)
+{
+    // recorrido de la lista
+    Nodo *actual; // referencia a nodo actual de la lista
+    cout << endl
+         << " VALORES ALMACENADOS EN LA LISTA " << endl;
+    actual = a.getPrimero(); // almacena temporalmente la posicion del primer nodo
+    while (actual != NULL)   // repetir mientras no se alcance el final de la lista
+    {
+        cout << "> " << actual->getDato() << endl;
+        actual = actual->getPunt(); // desplaza el puntero actual al siguiente nodo de la lista
+    }
+    cout << endl;
+}
