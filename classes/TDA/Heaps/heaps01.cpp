cdd1dfcb3f232703a5aa8211e47a753ebfba76c7
gokrusk <nigelljama@gmail.com>
2023-01-23 10:49:41 -0500

    heaps

--- classes/TDA/Heaps/heaps01.cpp
+++ classes/TDA/Heaps/heaps01.cpp
@@ -8,23 +8,23 @@
 int main(){											//programa principal
 	int n;
 	
-	n = leerN(1,20);								//cantidad de elementos
-	Monticulo m(n);									//creacion de monticulo
+	// n = leerN(1,20);								//cantidad de elementos
+	// Monticulo m(n);									//creacion de monticulo
 	
 	void leerMonticulo(Monticulo * m1, int n1);		//prototipo de la funcion que lee un monticulo de datos
 	void imprimirMonticulo(Monticulo m1, int n1);	//prototipo de la funcion que imprime un monticulo
 	void imprimirMinimo(Monticulo m1);				//prototipo de la funcion que imprime el valor minimo del monticulo
 	void eliminarMinimo(Monticulo *m1);				//prototipo de la funcion que elimina el valor minimo del monticulo y lo reordena
 	void ordenacionVectorHeaps();					//prototipo de la funcion que demuestra ordenacion de vector por medio de heaps
-
-	leerMonticulo(&m, n);							//llamada a ingreso de monticulo
-	imprimirMonticulo(m, n);						//llamada a impresion de monticulo
+	void merge2Heaps();
+	// leerMonticulo(&m, n);							//llamada a ingreso de monticulo
+	// imprimirMonticulo(m, n);						//llamada a impresion de monticulo
 
 	//imprimirMinimo(m);
 
 	//eliminarMinimo(&m);
 	//imprimirMonticulo(m, n);
-
+	merge2Heaps();
 	//ordenacionVectorHeaps();						//llama a funcion demostrativa de ordenamiento vectores => metodo 1
 	
 	cout << endl;
@@ -65,8 +65,8 @@ void eliminarMinimo(Monticulo *m1){				//funcion que elimina el valor minimo del
 }
 
 void ordenacionVectorHeaps(){					//funcion que demuestra ordenacion de vector por medio de heaps
-	cout << endl << "CANTIDAD DE ELEMENTOS A GENERAR";
-	int n = leerN(1, 10);
+	cout << endl << "CANTIDAD DE ELEMENTOS A GENERAR"<<endl;
+	int n = leerN(1, 20);
 
 	Monticulo m2(n);								//construye el monticulo
 
@@ -75,7 +75,7 @@ void ordenacionVectorHeaps(){					//funcion que demuestra ordenacion de vector p
 	
 	cout << endl << " V A L O R E S     O R I G I N A L E S" << endl;
 	for (int i = 0; i < n; i++){				//llena el vector con valores aleatorios
-		v0[i] = rand()%51;						//genera un valor aleatorio entre 0 y 50
+		v0[i] = rand()%51;						//genera un valor aleatorio entre 0 y 99
 		cout << " " << v0[i];					//genera un valor aleatorio
 	}
 
@@ -85,4 +85,32 @@ void ordenacionVectorHeaps(){					//funcion que demuestra ordenacion de vector p
 	for (int i = 0; i < n; i++){								//RECORRE el vector con valores ordenados
 		cout << " " << v0[i];
 	}
+}
+
+void merge2Heaps()
+{
+	Clave aux;
+	int t = 0;
+	int n = leerN(1,20);								//cantidad de elementos
+	Monticulo m1(n);									//creacion de monticulo
+	t+=n;
+	leerMonticulo(&m1,n);
+	n = leerN(1,20);								//cantidad de elementos
+	Monticulo m2(n);									//creacion de monticulo
+	leerMonticulo(&m2,n);
+	t+=n;
+	m1.setNumDatosMax(t);
+	for (int i = 0; i < m2.getNumDatosAct(); i++){
+		aux = m2.getValor(i);						//calcula valor extraido del monticulo
+		m1.Insertar(aux);
+	}
+	Monticulo m3(t);
+	for (int i = 0; i < t; i++)
+	{
+		aux = m1.BuscarMinimo();
+		m1.EliminarMinimo();
+		m3.Insertar(aux);
+	}
+	
+	imprimirMonticulo(m3,t);
 }
